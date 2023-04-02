fe6426fe5207fad0b519fc9581fb1a581be2be62
Gerwin Baula <baulagerwin@gmail.com>
2023-04-03 05:03:35 +0800

    startup db updated

--- startup/db.ts
+++ startup/db.ts
@@ -5,5 +5,5 @@ const dbUrl = process.env.DB_URL || (config.get("db") as string);
 
 export default function () {
   mongoose.set("strictQuery", false);
-  mongoose.connect(dbUrl).then(() => console.log(`Connected to ${dbUrl}...`));
+  mongoose.connect(dbUrl).then(() => console.info(`Connected to ${dbUrl}...`));
 }
